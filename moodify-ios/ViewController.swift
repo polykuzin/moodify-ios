dd9ebad413cd6200e2276c0406bad4a0e98ac789
polykuzin <polykuzin@gmail.com>
2022-03-07 16:28:21 +0300

    Initial Commit

---
+++ moodify-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  moodify-ios
+//
+//  Created by polykuzin on 07/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
